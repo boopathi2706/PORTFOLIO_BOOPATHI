5877d3954bfb2094caf14efe3b2972ca08a291e2
boopathi2706 <boopathi.v2023cse@sece.ac.in>
2025-01-12 11:53:05 +0530

    after theme verify commit

--- PORTFOLIO/main.js
+++ PORTFOLIO/main.js
@@ -5,7 +5,7 @@ const uiUxCount = document.getElementById("ui-ux-count");
 const certificateCount = document.getElementById("certificate-count");
 const internCount = document.getElementById("intern-count");
 
-let animated = false; // To ensure counters animate only once
+let animated = false; 
 
 // Function to animate the counter
 function animateCounter(element, maxCount) {
@@ -15,18 +15,18 @@ function animateCounter(element, maxCount) {
         element.textContent = count; 
         if (count >= maxCount) {
             element.textContent=count+"+";
-            clearInterval(interval); // Stop the interval once maxCount is reached
+            clearInterval(interval); 
         }
     }, 120);
 }
 
 countSection.addEventListener("mouseover", () => {
-    if (!animated) { // Check if animation has already occurred
+    if (!animated) { 
         animated = true;
-        animateCounter(fullstackCount, 10);  // Count up to 20 for Fullstack Projects
-        animateCounter(uiUxCount, 15);      // Count up to 15 for UI/UX Projects
-        animateCounter(certificateCount, 10); // Count up to 10 for Certificates
-        animateCounter(internCount, 2);     // Count up to 5 for Internships
+        animateCounter(fullstackCount, 10);  
+        animateCounter(uiUxCount, 15);     
+        animateCounter(certificateCount, 10); 
+        animateCounter(internCount, 2);    
     }
 });
 
@@ -68,8 +68,8 @@ function toggleIcon(element) {
 
 document.getElementById('downloadBtn').addEventListener('click', () => {
     const link = document.createElement('a');
-    link.href = './PORTFOLIO/img/BOOPATHI_V(RESUME).pdf'; // Replace with your PDF file path
-    link.download = 'BOOPATHI_VIJAYAN(RESUME).pdf'; // Optional: set the file name
+    link.href = './PORTFOLIO/img/BOOPATHI_V(RESUME).pdf';
+    link.download = 'BOOPATHI_VIJAYAN(RESUME).pdf'; 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
